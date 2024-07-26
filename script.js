12d9a66b0570212e8559232f9a44da692f22167f
Edgared123 <139364217+Edgared123@users.noreply.github.com>
2024-07-25 20:28:22 -0400

    >vvv><>

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('extractTable').addEventListener('click', function () {
     let passage = inputText.value;
 
     function replaceMatch(match, p1) {
-        const words = p1.trim().split(/\\s+/);
+        const words = p1.trim().split(/\s+/);
         if (words.length > 2) {
             return " "; // Return a space to ensure space between dialogues
         } else {
@@ -15,13 +15,13 @@ document.getElementById('extractTable').addEventListener('click', function () {
     passage = passage.replace(/[*—](.*?)[*—]/g, replaceMatch);
 
     // Remove extra spaces, including those at the end of the passage
-    passage = passage.replace(/\\s{2,}/g, ' ').trim();
+    passage = passage.replace(/\s{2,}/g, ' ').trim();
 
     // Remove all quotation marks from the processed passage
     passage = passage.replace(/"+/g, ' ').trim();
 
     // Ensure only single spaces throughout
-    passage = passage.replace(/\\s+/g, ' ');
+    passage = passage.replace(/\s+/g, ' ');
 
     inputText.value = passage; // Output back to the textarea
 });
